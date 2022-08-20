48c6283386c3c87ddb7d3fcf502218a0bc38cc69
YD-Cat <704200551@qq.com>
2022-08-21 00:48:59 +0800

    update 去掉无用方法

--- src/main/java/cn/ydcat/utils/md5/MD5Utils.java
+++ src/main/java/cn/ydcat/utils/md5/MD5Utils.java
@@ -32,17 +32,6 @@ public class MD5Utils {
     }
 
     /**
-     * MD5加密
-     * @param str 待加密字符串
-     * @param salt 盐
-     * @return 32位大写MD5
-     */
-    public static String encodeToMD5(String str,String salt) {
-        str+="YD-Cat"+salt;
-        return encodeToMD5(str);
-    }
-
-    /**
      * 判断某字符串是否和某MD5相等
      * @param str 未加密字符串
      * @param MD5 MD5
@@ -54,16 +43,4 @@ public class MD5Utils {
         return MD5.toUpperCase().equals(encodeToMD5(str));
     }
 
-    /**
-     * 判断某字符串用盐加密后是否和某MD5相等
-     * @param str 未加密字符串
-     * @param salt 盐
-     * @param MD5 MD5
-     * @return
-     */
-    public static boolean isEquals(String str,String salt, String MD5){
-        if(str==null || "".equals(str) || MD5==null || "".equals(MD5))
-            return false;
-        return MD5.toUpperCase().equals(encodeToMD5(str,salt));
-    }
 }
